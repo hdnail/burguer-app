ce174809b9385c3ffb5d34ecf47db88c50eeed02
Damian Nail <hdnail@gmail.com>
2020-06-07 19:24:12 -0300

    Refactors burguer-app folder structure

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
 
 function App() {
